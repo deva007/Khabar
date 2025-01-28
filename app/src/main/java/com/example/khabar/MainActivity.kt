14c079bf16f071896dc4d387debe615e3e2359e9
deva007 <devashish111997@gmail.com>
2025-01-28 14:55:09 +0530

    init commit 3

--- app/src/main/java/com/example/khabar/MainActivity.kt
+++ app/src/main/java/com/example/khabar/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             KhabarTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android boy",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
